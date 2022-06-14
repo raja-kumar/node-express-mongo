4b9eb4e5a685fd57c2bda8b477e052158df960bf
raja-kumar1 <raja.k@thesilverlabs.com>
2022-06-14 12:19:15 +0530

    fixed the signup/login issue

--- markdown-blog/server.js
+++ markdown-blog/server.js
@@ -4,16 +4,23 @@ const articleRouter = require('./routes/articles');
 const userAuthRouter = require('./routes/user_auth');
 const mongoose = require('mongoose');
 const Article = require('./models/article');
+var session = require('express-session');
 const methodOverride = require('method-override');
 
 app.set('view engine', 'ejs');
+app.use(session({secret: "Your secret key"}));
 
 //connect to database
 mongoose.connect('mongodb://localhost/my_db');
 
 app.get('/', async function(req, res){
     let articles = await Article.find().sort({createdAt : "desc"});
-    res.render('articles/index' , {articles: articles});
+    if (req.session.user){
+        res.render('user_auth/index_login', {articles: articles, user : req.session.user});
+    }
+    else{
+        res.render('articles/index' , {articles: articles});
+    }
 });
 
 
